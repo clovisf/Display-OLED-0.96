3be1ae21d3976cf13235a9aeb525252ea8b37cab
Clovis Fritzen <clovisf@gmail.com>
2024-06-15 19:51:57 -0300

    added image

--- teste-display-oled-0.96/teste-display-oled-0.96.ino
+++ teste-display-oled-0.96/teste-display-oled-0.96.ino
@@ -1,5 +1,6 @@
 #include <C:\Users\Clovisf\Documents\GitHub\Display-OLED-0.96\teste-display-oled-0.96\DigisparkOLED.h>
 #include <Wire.h>
+#include "simoni.h"
 // ============================================================================
 
 void setup() 
@@ -11,19 +12,18 @@ void setup()
 void loop()
 {
   // put your main code here, to run repeatedly:
-  oled.fill(0xFF); //fill screen with color
-  delay(1000);
+  //oled.fill(0x00); //fill screen with color
+  //delay(1000);
   oled.clear(); //all black
-  delay(1000);
-  //usage: oled.setCursor(X IN PIXELS, Y IN ROWS OF 8 PIXELS STARTING WITH 0);
-  oled.setCursor(0, 0); //top left
-  oled.setFont(FONT8X16);
-  oled.print(F("DIGISTUMP")); //wrap strings in F() to save RAM!
-  oled.setFont(FONT6X8);
-  oled.print(F(" OLED!"));
-  oled.setCursor(0, 2); //two rows down because the 8x16 font takes two rows of 8
-  oled.println(F("test")); //println will move the cursor 8 or 16 pixels down (based on the front) and back to X=0
-  oled.print(F("test test test test test")); //lines auto wrap
-  
+  //delay(100);
+  oled.bitmap(0, 0, 128, 8, simoni);
   delay(3000);
+  //usage: oled.setCursor(X IN PIXELS, Y IN ROWS OF 8 PIXELS STARTING WITH 0);
+  /*oled.setCursor(0, 0); //top left
+  oled.setFont(FONT8X16);
+  oled.print(F("FritzenLab")); //wrap strings in F() to save RAM!
+  oled.setFont(FONT6X8);
+  oled.setCursor(0, 2); //two rows down because the 8x16 font takes two rows of 8
+  oled.print(F("This is a text test from FritzenLab")); //lines auto wrap*/
+  
 }
