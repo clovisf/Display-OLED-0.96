5f4fccb6c628510c8742542d71e0388550e7b3c1
Clovis Fritzen <clovisf@gmail.com>
2024-06-17 22:11:35 -0300

    added images and gif

--- teste-display-oled-0.96/teste-display-oled-0.96.ino
+++ teste-display-oled-0.96/teste-display-oled-0.96.ino
@@ -1,29 +1,32 @@
 #include <C:\Users\Clovisf\Documents\GitHub\Display-OLED-0.96\teste-display-oled-0.96\DigisparkOLED.h>
 #include <Wire.h>
-#include "simoni.h"
 // ============================================================================
-
+int counter= 0;
 void setup() 
 {
   // put your setup code here, to run once:
   oled.begin();
+  oled.clear(); //all black
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  //oled.fill(0x00); //fill screen with color
-  //delay(1000);
-  oled.clear(); //all black
-  //delay(100);
-  oled.bitmap(0, 0, 128, 8, simoni);
-  delay(3000);
-  //usage: oled.setCursor(X IN PIXELS, Y IN ROWS OF 8 PIXELS STARTING WITH 0);
-  /*oled.setCursor(0, 0); //top left
-  oled.setFont(FONT8X16);
-  oled.print(F("FritzenLab")); //wrap strings in F() to save RAM!
+  delay(1000);
+  counter++; 
+  oled.setCursor(0, 0); //top left
   oled.setFont(FONT6X8);
+  oled.print(F("This is a text test")); //lines auto wrap
+  oled.setCursor(0, 1);
+  oled.print(F("from FritzenLab")); //lines auto wrap
+  oled.setFont(FONT8X16);
   oled.setCursor(0, 2); //two rows down because the 8x16 font takes two rows of 8
-  oled.print(F("This is a text test from FritzenLab")); //lines auto wrap*/
-  
+  oled.print(F("FritzenLab.net")); //wrap strings in F() to save RAM!
+  oled.setFont(FONT6X8);
+  oled.setCursor(0, 4); //two rows down because the 8x16 font takes two rows of 8
+  oled.print(F("FritzenLab.com.br")); //wrap strings in F() to save RAM!
+  oled.setFont(FONT8X16);
+  oled.setCursor(10, 5); //two rows down because the 8x16 font takes two rows of 8
+  oled.print(counter); //wrap strings in F() to save RAM!
+
+  //delay(3000);
 }
